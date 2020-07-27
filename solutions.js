30998c1f70112d527d9be750f5d4eba8e8cf382f
Robert De la Rosa <rdelarosa39@gmail.com>
2020-07-27 16:37:33 -0500

    completed 1-9

--- solutions.js
+++ solutions.js
@@ -20,3 +20,56 @@
  * and
  * concat
  */
+// 1. isTrue
+function isTrue(arg){
+    return arg === true;
+}
+
+// 2. isFalse
+function isFalse(arg){
+    return arg === false;
+}
+
+// 3. not
+function not(arg){
+    return !arg;
+}
+// 4. addOne
+function addOne(arg){
+    return !isNaN(parseInt(arg)) ? parseInt(arg) + 1 : arg;
+}
+
+// 5. isEven
+function isEven(arg){
+    return parseInt(arg)%2 === 0;
+}
+
+// 6. isIdentical
+function isIdentical(arg,arg1){
+    return arg === arg1;
+}
+
+// 7. isEqual
+function isEqual(arg,arg1){
+    return arg == arg1;
+}
+
+// 8. or
+function or(arg,arg1){
+    return arg || arg1;
+}
+
+// 9 and
+function and(arg,arg1){
+    return arg && arg1;
+}
+
+// 10 concat
+function concat(arg,arg1){
+    if(typeof(arg)==typeof(arg1)){
+        return arg + arg1;
+    }
+    else if(!isNaN(arg) && !isNaN(arg1)){
+        return arg.toString() + arg1.toString();
+    }
+}
